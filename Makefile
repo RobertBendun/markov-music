0f7a853e32369d9de16a9dc6239c5efaebe5d88b
Robert Bendun <robert@bendun.cc>
2021-11-18 18:42:14 +0100

    key2music.cc: support for changing only specified column

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX=g++
-CXXFLAGS=-Wall -Wextra -O3 -std=c++17
+CXXFLAGS=-Wall -Wextra -O3 -std=c++20
 OUTPUT=graph-markov key2music random-markov-path midi/bigrams-notes midi/bigrams-dur
 
 .PHONY: all
--- key2music.cc
+++ key2music.cc
@@ -1,18 +1,58 @@
 #include <iostream>
+#include <charconv>
+#include <cstring>
+#include <cassert>
+#include <ranges>
+#include <algorithm>
 
-int main()
+int main(int argc, char **argv)
 {
+	unsigned column = 0;
+	if (argc >= 2) {
+		auto [p, e] = std::from_chars(argv[1], argv[1] + std::strlen(argv[1]), column);
+		if (p == argv[1]) {
+			std::cerr << "cannot parse column number '" << argv[1] << "'\n";
+			return 1;
+		}
+	}
+
 	constexpr static char const* notes[] = {
 		"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B"
 	};
 
-	while (std::cin) {
-		unsigned key;
-		std::cin >> key;
+	if (column == 0) {
+		while (std::cin) {
+			unsigned key;
+			std::cin >> key;
+			auto octave = key / 12, note = key % 12;
+			std::cout << notes[note] << '\t' << octave << '\n';
+		}
+		return 0;
+	}
 
-		auto octave = key / 12;
-		auto note = key % 12;
+	for (std::string line; std::getline(std::cin, line); ) {
+		auto n = column;
+		bool fst = true;
+		for (auto const column_range : std::views::split(line, '\t')) {
+			if (fst) {
+				fst = false;
+			} else {
+				std::cout << '\t';
+			}
+			std::string column;
+			std::ranges::copy(column_range, std::back_inserter(column));
 
-		std::cout << notes[note] << '\t' << octave << '\n';
+			if (--n == 0) {
+				unsigned key = 0;
+				auto [ptr, _] = std::from_chars(column.data(), column.data() + column.size(), key);
+				assert(ptr != column.data());
+
+				auto octave = key / 12, note = key % 12;
+				std::cout << notes[note] << '\t' << octave;
+			} else {
+				std::cout << column;
+			}
+		}
+		std::cout << '\n';
 	}
 }
